48daac755a76a3fbe70a07960d3692a2eadd77ec
JARED DE LA O HERNANDEZ <jdeo@ad.elektra.com.mx>
2019-12-10 18:27:35 -0600

    animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Power2, Power3, TweenMax, CSSPlugin } from 'gsap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('box', { static: false }) box: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.do();
+  }
+
+  do() {
+    TweenMax.to(this.box.nativeElement, 3, {
+      css: {
+        delay: 1,
+        opacity: 0.7,
+        height: '100px',
+        backgroundColor: 'white',
+        ease: Power3.easeInOut
+      }
+    });
   }
 
 }
